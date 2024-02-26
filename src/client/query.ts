9dec1d56c3eee1233cb43a12925e6b70de626194
Ramiro Carlucho <ramirocarlucho@gmail.com>
2024-02-26 15:45:47 -0300

    change query

--- src/client/query.ts
+++ src/client/query.ts
@@ -4,7 +4,7 @@ export async function query(
   endpoint: string,
   url = 'http://127.0.0.1:1317'
 ): Promise<unknown> {
-  const res = await fetch(`${url}/${endpoint}`, {
+  const res = await fetch(`${url}${endpoint}`, {
     headers: { 'Content-Type': 'application/json' }
   });
   return res.json();
